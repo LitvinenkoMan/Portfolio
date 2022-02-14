b0289e7d3973889e9229760a1a03c46f3118de56
LitvinenkoMan <71625297+LitvinenkoMan@users.noreply.github.com>
2022-02-14 19:27:10 +0300

    Creating Networking

---
+++ Project/Summary/Assets/Scripts/Network/PlayerHost.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerHost : MonoBehaviour
+{
+    //public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
