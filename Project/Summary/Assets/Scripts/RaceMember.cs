7084575895a4bd342f6ef649976a0fc2ed98acb1
LitvinenkoMan <71625297+LitvinenkoMan@users.noreply.github.com>
2022-03-07 21:07:27 +0300

    Race member updated

--- Project/Summary/Assets/Scripts/RaceMember.cs
+++ Project/Summary/Assets/Scripts/RaceMember.cs
@@ -6,16 +6,20 @@ using UnityEngine;
 
 public class RaceMember : MonoBehaviour, IRacer
 {
+    [SerializeField] private float raundsLeft = 1;
     [SerializeField] private RaceCheckPointScript[] CheckPoints;
-    [SerializeField] private GameObject TextContainer;    
+    [SerializeField] private GameObject TextContainer;
     [SerializeField] private TextMeshProUGUI TimerText;
     private bool _playerStartedRace;
+    private bool _CheckPointCrossed;
     private float _timer;
-    
+    private float _afterCheckpointTimer;
+
     void Start()
     {
         _playerStartedRace = false;
         _timer = 0;
+        _afterCheckpointTimer = 5;
     }
 
     void Update()
@@ -24,6 +28,29 @@ public class RaceMember : MonoBehaviour, IRacer
         {
             RaceTimer();
             ShowTimerValues();
+        }
+
+        if (_CheckPointCrossed)
+        {
+            _afterCheckpointTimer -= Time.deltaTime;
+            if (_afterCheckpointTimer < 0)
+            {
+                _afterCheckpointTimer = 5;
+                if (raundsLeft < 0)
+                {
+                    TextContainer.SetActive(false);
+                    _timer = 0;
+                    raundsLeft = 1;
+                }
+
+                _CheckPointCrossed = false;
+            }
+        }
+
+        if (raundsLeft < 0)
+        {
+            _playerStartedRace = false;
+            _afterCheckpointTimer = 5;
         }
     }
 
@@ -44,6 +71,12 @@ public class RaceMember : MonoBehaviour, IRacer
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.GetComponent<Race>() && _playerStartedRace && !_CheckPointCrossed)
+        {
+            raundsLeft -= 1;
+            _CheckPointCrossed = true;
+        }
+
         if (other.GetComponent<Race>() && !_playerStartedRace)
         {
             _playerStartedRace = true;
